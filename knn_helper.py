88f261591030b3a4cc4bfcc13bc563c4784e0541
clovis <clovisgladstone@gmail.com>
2011-06-29 12:25:26 -0500

    add ability to search for pregenerated results in sqlite

--- knn_helper.py
+++ knn_helper.py
@@ -9,38 +9,48 @@ from word_mapper import mapper
 
 class knn(object):
     
-    def __init__(self, doc, path, docs=None, top_words=0, lower_words=-1, metric=cosine):
-        self.id_to_word = mapper(path + '/WORK/all.frequencies')
-        self.top = top_words
-        self.lower = lower_words
-        self.orig = self.loader(doc, path, top_words, lower_words)
-        self.metric = metric
-        self.path = path
-        self.results = []
-        if docs == None:
-            self.docs = doc_enumerator(path)
+    def __init__(self, db_path, doc=None, docs=None, stored=True, top_words=0, lower_words=-1, metric=cosine):
+        self.id_to_word = mapper(db_path + '/WORK/all.frequencies')
+        self.stored = stored
+        if stored:
+            self.db = db_path + 'vsm_results.sqlite'
+            self.conn = sqlite3.connect(self.db)
+            self.cursor = self.conn.cursor()
         else:
-            self.docs = docs
+            self.top = top_words
+            self.lower = lower_words
+            self.orig = self.loader(doc, db_path, top_words, lower_words)
+            self.metric = metric
+            self.path = db_path
+            self.results = []
+            if docs == None:
+                self.docs = doc_enumerator(path)
+            else:
+                self.docs = docs
             
-    def search(self):
-        self.__call__()
-        
-    def __call__(self):
-        for doc_id in self.docs:
-            doc = np_array_loader(doc_id, self.path, top=self.top, lower=self.lower)
-            self.results.append((doc_id, 1 - self.distance(doc)))
-            
+    def search(self, doc_id, display=10):
+        if self.stored:
+            query = 'select neighbor_doc_id, neighbor_distance from results where doc_id = %d order by neighbor_distance desc limit %d' % (doc_id, display)
+            self.cursor.execute(query)
+            self.results = self.cursor.fetchall()
+            return self.results
+        else:
+            for doc_id in self.docs:
+                doc = np_array_loader(doc_id, self.path, top=self.top, lower=self.lower)
+                self.results.append((doc_id, 1 - self.distance(doc)))
+            return self.results
+    
     def distance(self, doc):
         return 1 - self.metric(self.orig, doc)
         
     def loader(self, doc, path, top_words, lower_words):
         return np_array_loader(doc, path, top=top_words, lower=lower_words)
         
-    def sort_results(self, order=True, display=-1):
+    def sort_results(self, results, order=True, display=-1):
         return sorted(self.results, key=itemgetter(1), reverse=order)[:display]
         
     def word_freq_sort(self, doc, path):
         # exclude last column in row since it doesn't exist in word mappings
         mydoc = np_array_loader(doc, path)
         frequencies = [(self.id_to_word[word], mydoc[word]) for word in mydoc[:-1].nonzero()[0]]
-        return sorted(frequencies, key=itemgetter(1), reverse=True)[100:110]
+        return sorted(frequencies, key=itemgetter(1), reverse=True)[:10]
